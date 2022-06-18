26d1902b03d55e2b787f845502dad67b845a1af4
Gunfireinside <2004240015hs@edu.oca.ac.jp>
2022-06-18 14:34:36 +0900

    test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 
 app = Flask(__name__)
 @app.route("/")
@@ -16,3 +16,15 @@ def test_request():
 @app.route("/exercise_request/<value>")
 def exercise_request(value):
     return f"{value}"
+
+@app.route("/show_html")
+def show_html():
+    return render_template("test.html")
+
+@app.route("/show_html2")
+def show2_html():
+    return render_template("test2.html")
+
+@app.route("/exercise")
+def exercise_name():
+    return f'value:{request.args.get("my_name")}'
