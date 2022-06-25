9bca4278462934ff762fdfc6983531e11ca0c406
oca-takuto <2004240016ht@edu.oca.ac.jp>
2022-06-25 17:40:37 +0900

    pdas

--- test_model.py
+++ test_model.py
@@ -7,6 +7,15 @@ Base = declarative_base()
 metadata = Base.metadata
 
 
+class Human(Base):
+    __tablename__ = 'human'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Text, nullable=False)
+    height = Column(Float)
+    weight = Column(Float)
+
+
 class Person(Base):
     __tablename__ = 'person'
 
